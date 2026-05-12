f03fba16f2a054c0c71fd05af32f28c454b19e4b
amina <aminaM@gmail.com>
2026-05-12 12:30:49 +0100

    'm'

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,6 @@
-
-from django.urls import path
-from users import views
+from django.urls import path, include
 
 urlpatterns = [
-    path('',            views.get_utilisateurs,   name='get_utilisateurs'),
-    path('create/',     views.create_utilisateur, name='create'),
-    path('<int:pk>/',   views.get_utilisateur, name='getbyid'),
-    path('<int:pk>/edit/',   views.update_utilisateur, name='update'),
-    path('<int:pk>/delete/', views.delete_utilisateur, name='delete'),
+    path('api/', include('users.urls')),        # tes routes API → /api/
+    path('', include('users.html_urls')),       # routes HTML → /
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from users import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/utilisateurs/', include('users.urls')),   # tes routes API existantes
-    path('utilisateurs/', include('users.html_urls')),  # nouvelles routes HTML
+    path('',                   views.get_utilisateurs,   name='get_utilisateurs'),
+    path('create/',            views.create_utilisateur, name='create'),
+    path('<int:pk>/',          views.get_utilisateur,    name='getbyid'),
+    path('<int:pk>/edit/',     views.update_utilisateur, name='update'),
+    path('<int:pk>/delete/',   views.delete_utilisateur, name='delete'),
 ]
